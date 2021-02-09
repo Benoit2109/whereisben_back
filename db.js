a738567a9c03cbf4a11c657c0bcd8deaa1d2fd38
Benoit LE BOURDONNEC <benoitlebourdonnec@MacBook-Pro-2.local>
2021-02-09 09:22:35 +0100

    new connection

--- db.js
+++ db.js
@@ -1,29 +1,17 @@
-const mysql = require('mysql');
-require('dotenv').config();
-
-let connection;
-
-function getConnection() {
-  if (!connection) {
-    connection = mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
-  }
-  if (connection && connection.state === 'disconnected') {
-    connection.connect(function (err) {
-      if (err) {
-        console.error(`error connecting: ${err.stack}`);
-        return;
-      }
-
-      console.log(`connected as id ${connection.threadId}`);
-    });
-  }
-
-  return connection;
-}
-
 module.exports = getConnection();
+
+
+require("dotenv").config();
+const mysql = require("mysql");
+
+const config = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  timezone: 'Europe/Paris'
+};
+const connection = mysql.createPool(config);
+
+module.exports = connection;
